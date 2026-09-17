78560350d2669587a958b0964749e865793c5129
ponystoosty <tho.giotti@gmail.com>
2026-09-18 00:33:23 +0200

    add 404 and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   // Remplacer par le nom de domaine définitif du site avant mise en production.
   site: 'https://www.exemple-cabinet.fr',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
